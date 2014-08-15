3d9cb825015b2494461986f66e0643395e666ccf
Adam PS <labonvie07@gmail.com>
2014-08-15 16:21:44 -0400

    Update svg.js

--- _includes/svg.js
+++ _includes/svg.js
@@ -3,6 +3,6 @@
     function supportsSVG() {
         return !!document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect
     }
-    if (supportsSVG()) document.body.className += 'svg'
+    if (supportsSVG()) document.documentElement.className += 'svg'
 }()
 </script>
