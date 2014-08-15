b9d29e1ac762cc65f47d30fbeedbe9c693e1f5c4
Adam PS <labonvie07@gmail.com>
2014-08-15 15:51:41 -0400

    Update svg.js

--- _includes/svg.js
+++ _includes/svg.js
@@ -1,6 +1,8 @@
+<script>
 ! function () {
     function supportsSVG() {
         return !!document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect
     }
     if (supportsSVG()) document.body.className += ' svg'
 }()
+</script>
