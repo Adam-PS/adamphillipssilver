379af2f2b3cdd6068fa3391a28b408f56d97d4fb
Adam PS <labonvie07@gmail.com>
2014-08-15 15:36:16 -0400

    Create svg.js

---
+++ _includes/svg.js
@@ -0,0 +1,6 @@
+! function () {
+    function supportsSVG() {
+        return !!document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect
+    }
+    if (supportsSVG()) document.body.className += ' svg'
+}()
